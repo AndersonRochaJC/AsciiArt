8340a72f2c46bd565369779eb819d86349e06023
AndersonRochaJC <jmcreggae@gmail.com>
2021-08-27 21:36:53 -0300

    exercicio concluido

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("AsciiArt");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("  || ||");
+        Console.WriteLine("\\\\ ( ) //");
+        Console.WriteLine("//(___)\\\\");
+        Console.WriteLine(" ||   ||");
+        Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
